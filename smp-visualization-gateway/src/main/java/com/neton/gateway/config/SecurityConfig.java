2cd4b69ed13a12ec421e88d7d9f9c9844c134625
TheSunshine <TheSunshine>
2024-10-25 18:19:59 +0800

    feat：修改网关校验

--- smp-visualization-gateway/src/main/java/com/neton/gateway/config/SecurityConfig.java
+++ smp-visualization-gateway/src/main/java/com/neton/gateway/config/SecurityConfig.java
@@ -1,12 +1,39 @@
 package com.neton.gateway.config;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * @author TheSunshine
@@ -14,17 +41,107 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
  */
 @Configuration
 @EnableWebFluxSecurity
+@Slf4j
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-      return null;
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+
+        http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/public/**").permitAll()  // 公开路径
+                        .anyExchange().authenticated()           // 其他路径需要认证
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtDecoder(reactiveJwtDecoder())           // 使用自定义的 JwtDecoder
+                        )
+                );
+
+        return http.build();
     }
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        RSAPublicKey publicKey = (RSAPublicKey) rsaKeyPair().getPublic();
+        return NimbusReactiveJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        // 替换为你的JWT签名密钥
-        String jwkSetUri = "http://your-auth-server/.well-known/jwks.json";
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+        JwtDecoder jwtDecoder = jwtDecoder(jwkSource());
+        return jwtDecoder;
+    }
+
+    public static JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        Set<JWSAlgorithm> jwsAlgs = new HashSet<>();
+        jwsAlgs.addAll(JWSAlgorithm.Family.RSA);
+        jwsAlgs.addAll(JWSAlgorithm.Family.EC);
+        jwsAlgs.addAll(JWSAlgorithm.Family.HMAC_SHA);
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+        JWSKeySelector<SecurityContext> jwsKeySelector =
+                new JWSVerificationKeySelector<>(jwsAlgs, jwkSource);
+        jwtProcessor.setJWSKeySelector(jwsKeySelector);
+        // Override the default Nimbus claims set verifier as NimbusJwtDecoder handles it instead
+        jwtProcessor.setJWTClaimsSetVerifier((claims, context) -> {
+        });
+        return new NimbusJwtDecoder(jwtProcessor);
+    }
+
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = rsaKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    public KeyPair rsaKeyPair()  {
+        // 从文件中读取私钥
+        PrivateKey privateKey = loadPrivateKey("key/private.key");
+
+        // 从文件中读取公钥
+        PublicKey publicKey = loadPublicKey("key/public.key");
+
+        return new KeyPair(publicKey, privateKey);
+    }
+
+    private PrivateKey loadPrivateKey(String fileName) {
+        try {
+            byte[] keyBytes = loadKeyFromFile(fileName);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePrivate(spec);
+        }catch (Exception e){
+            log.error("获取私钥失败 ====================> :{}",e.getMessage());
+        }
+        return null;
+    }
+
+    private PublicKey loadPublicKey(String fileName) {
+        try {
+            byte[] keyBytes = loadKeyFromFile(fileName);
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePublic(spec);
+        }catch (Exception e){
+            log.error("获取公钥失败===================> :{}",e.getMessage());
+        }
+        return null;
+    }
+
+    private byte[] loadKeyFromFile(String fileName) throws IOException {
+        org.springframework.core.io.Resource resource = new ClassPathResource(fileName);
+        try (InputStream inputStream = resource.getInputStream()) {
+            return inputStream.readAllBytes();
+        }
     }
 }
